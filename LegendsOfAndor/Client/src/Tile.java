25fd42a2b4632d2d49e3f95008574e43b8c4765f
test <test@test-PC>
2020-03-05 20:00:19 -0500

    added tiles 10-20

--- LegendsOfAndor/Client/src/Tile.java
+++ LegendsOfAndor/Client/src/Tile.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 public class Tile {
     // TODO Make Tile Class Flyweight
     // TODO Make accessor to get tile from index
-    private MinuetoWindow screen;
+    private static MinuetoWindow screen;
     private ArrayList<Coordinate> coords;
     private ArrayList<TileEntity> tileEntities;
     private ArrayList<Character> tileCharacters;
@@ -22,10 +22,10 @@ public class Tile {
     private int nextTile;
     private static final ArrayList<Tile> TILES = new ArrayList<Tile>();
 
-    public Tile(int moveX, int moveY, int tileNumber, int[] adjacentTiles) {
+    public Tile(int moveX, int moveY, int tileNumber, int[] adjacentTilesn, int nextTile) {
         this.coords = new ArrayList<>();
         this.moveCoords = new Coordinate(moveX, moveY);
-        this.screen = screen;
+//        this.screen = screen;
         this.tileEntities = new ArrayList<>();
         this.tileCharacters = new ArrayList<>();
         this.adjacentTiles = adjacentTiles;
@@ -36,20 +36,33 @@ public class Tile {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+        this.nextTile = nextTile;
     }
     
     static {
-    	TILES.add(new Tile(1353, 1439, 0, new int[] {1, 2, 4, 5, 6, 7, 11}));
-    	TILES.add(new Tile(1450, 2225, 1, new int[] {0, 2, 3, 4}));
-    	TILES.add(new Tile(2043,2205, 2, new int[] {0, 1, 3, 6, 14}));
-    	TILES.add(new Tile(1800, 2685, 3, new int[] {1, 2, 4, 10, 14, 19, 20}));
-    	TILES.add(new Tile(994, 2408, 4, new int[] {0, 1, 3, 5, 20, 21}));
-    	TILES.add(new Tile(378, 2256, 5, new int[] {0, 4, 21}));
-    	TILES.add(new Tile(2725, 2079, 6, new int[] {0, 2, 11, 13, 14, 17}));
-    	TILES.add(new Tile(1289, 784, 7, new int[] {0, 8, 9, 11, 15}));
-    	TILES.add(new Tile(2095, 866, 8, new int[] {7, 9, 11}));
-    	TILES.add(new Tile(1551, 283, 9, new int[] {7, 8, 15}));
-    	TILES.add(new Tile(2028, 3074, 10, new int[] {3, 14, 18, 19}));        
+    	TILES.add(new Tile(1353, 1439, 0, new int[] {1, 2, 4, 5, 6, 7, 11}, 0));
+    	TILES.add(new Tile(1450, 2225, 1, new int[] {0, 2, 3, 4}, 0));
+    	TILES.add(new Tile(2043,2205, 2, new int[] {0, 1, 3, 6, 14}, 0));
+    	TILES.add(new Tile(1800, 2685, 3, new int[] {1, 2, 4, 10, 14, 19, 20}, 1));
+    	TILES.add(new Tile(994, 2408, 4, new int[] {0, 1, 3, 5, 20, 21}, 0));
+    	TILES.add(new Tile(378, 2256, 5, new int[] {0, 4, 21}, 0));
+    	TILES.add(new Tile(2725, 2079, 6, new int[] {0, 2, 11, 13, 14, 17}, 0));
+    	TILES.add(new Tile(1289, 784, 7, new int[] {0, 8, 9, 11, 15}, 0));
+    	TILES.add(new Tile(2095, 866, 8, new int[] {7, 9, 11}, 7));
+    	TILES.add(new Tile(1551, 283, 9, new int[] {7, 8, 15}, 7));
+    	TILES.add(new Tile(2028, 3074, 10, new int[] {3, 14, 18, 19}, 3));
+    	TILES.add(new Tile(1024, 620, 11, new int[] {0, 6, 7, 8, 12, 13}, 0));
+    	TILES.add(new Tile(1356, 660, 12, new int[] {11, 13}, 11));
+    	TILES.add(new Tile(1656, 848, 13, new int[] {6, 11, 12, 16, 17}, 12));
+    	TILES.add(new Tile(1112, 1236, 14, new int[] {2, 3, 6, 10, 17, 18}, 2));
+    	TILES.add(new Tile(368, 104, 15, new int[] {7, 9}, 7));
+    	TILES.add(new Tile(1872, 1176, 16, new int[] {13, 17, 36, 38, 32}, 13));
+    	TILES.add(new Tile(1516, 1244, 17, new int[] {6, 14, 14, 16, 18, 36}, 6));
+    	TILES.add(new Tile(1212, 1576, 18, new int[] {10, 14, 17, 19, 28, 36, 72}, 14));
+    	TILES.add(new Tile(668, 1440, 19, new int[] {3, 10, 18, 20, 22, 23, 72}, 3));
+    	TILES.add(new Tile(380, 1348, 20, new int[] {3, 4, 19, 21, 22}, 3));
+    	
+ 
     }
     
     public static Tile get (int index) {
@@ -62,7 +75,7 @@ public class Tile {
     }
     
     public void setScreen(MinuetoWindow screen) {
-    	this.screen = screen;
+    	Tile.screen = screen;
     }
     
     public void setup(int x, int y, int next) {
@@ -98,11 +111,11 @@ public class Tile {
     }
 
     public void draw() {
-        Coordinate coordinates;
-        for(int i = 0; i < tileEntities.size(); i++) {
-            coordinates = camera.getPosOnScreen(coords.get(0));
-            this.screen.draw(tileEntities.get(i).getImage(), coordinates.getX(), coordinates.getY());
-        }
+//        Coordinate coordinates;
+//        for(int i = 0; i < tileEntities.size(); i++) {
+//            coordinates = camera.getPosOnScreen(coords.get(0));
+//            Tile.screen.draw(tileEntities.get(i).getImage(), coordinates.getX(), coordinates.getY());
+//        }
     }
     public boolean isAdjacent(int adjacentTile) {
         for(int tileNum : adjacentTiles)
