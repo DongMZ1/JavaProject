906177f97dc3a0db6763b25ae338abf7ca17f5ed
DongMZ1 <46273365+DongMZ1@users.noreply.github.com>
2020-03-08 14:08:05 -0400

    Add files via upload

---
+++ LegendsOfAndor/Client/src/Wineskin.java
@@ -0,0 +1,25 @@
+import org.minueto.MinuetoFileException;
+import org.minueto.image.MinuetoImage;
+import org.minueto.image.MinuetoImageFile;
+
+public class Wineskin implements Item {
+
+	MinuetoImage wineskin; 
+	int tile;
+	
+	public Wineskin(int tile) throws MinuetoFileException {
+		this.wineskin = new MinuetoImageFile("images/wineskin.png");
+		this.tile = tile;
+		// TODO Auto-generated constructor stub
+	}
+	 public void setTile(int tile) {
+	        this.tile = tile;
+	    }
+	    public int getTile() {
+	        return this.tile;
+	    }
+	public MinuetoImage getImage() {
+     return this.wineskin;
+ }
+
+}
