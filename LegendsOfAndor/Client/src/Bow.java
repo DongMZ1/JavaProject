90b1a4064ab04a18859cc148deb56816c744126a
yliu306 <yliu306@open-4.CS.McGill.CA>
2020-03-09 10:58:34 -0400

    added bow class

---
+++ LegendsOfAndor/Client/src/Bow.java
@@ -0,0 +1,33 @@
+import org.minueto.MinuetoFileException;
+import org.minueto.image.MinuetoImage;
+import org.minueto.image.MinuetoImageFile;
+
+public class Bow implements Item{
+	int Tile;
+	MinuetoImage image;
+	
+	public void Bowin(int tile) throws MinuetoFileException{
+		Tile = tile;
+		image = new MinuetoImageFile("images/wineskin.png");
+	}
+	
+	
+	@Override
+	public MinuetoImage getImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getTile() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setTile(int tile) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
