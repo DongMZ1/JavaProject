a3b812da87eddee4c3914db9f88d0adfdcd8ff7a
jargonless <43076948+jargonless@users.noreply.github.com>
2020-03-11 19:53:14 -0400

    Add files via upload

--- LegendsOfAndor/Client/src/PlayerBoard.java
+++ LegendsOfAndor/Client/src/PlayerBoard.java
@@ -1,23 +1,93 @@
+import java.io.IOException;
+
+import org.minueto.MinuetoColor;
 import org.minueto.MinuetoFileException;
+import org.minueto.image.MinuetoFont;
 import org.minueto.image.MinuetoImage;
 import org.minueto.image.MinuetoImageFile;
+import org.minueto.image.MinuetoRectangle;
+import org.minueto.image.MinuetoText;
 
 public class PlayerBoard {
     private static PlayerBoard playerBoard;
-
-    private MinuetoImage playerBoardImage = new MinuetoImageFile("images/Heroes/WarriorMaleBoard.png").scale(300/ 671.0, 300/ 671.0);
-
-    public PlayerBoard() throws MinuetoFileException {
-
+    private static GameStatus gameStatus;
+    private boolean flag = false;
+    
+    private int[] spX = {620};
+    int sp;
+    
+    int wp;
+    private int[] wpX = {730};
+    private int[] wpY = {260};
+    
+    private String image;
+    //private MinuetoImage playerBoardImage = new MinuetoRectangle(width, height, MinuetoColor.BLACK, true);
+    private MinuetoImage playerBoardImage;
+    private MinuetoImage spImage = new MinuetoImageFile("images/tokenWarrior.png");
+    private MinuetoImage wpImage = new MinuetoImageFile("images/tokenWarrior.png");
+    
+   
+    
+    private PlayerBoard(Hero hero) throws IOException {
+    	gameStatus = GameStatus.getInstance();
+    	
+    	sp = hero.sp;
+    	wp = hero.wp;
+    	
+    	if(hero instanceof Warrior) {
+    		image = "images/Heroes/WarriorMaleBoard.png";
+    	}
+    	
+    	else if(hero instanceof Archer) {
+    		image = "images/Heroes/ArcherMaleBoard.png";
+    	}
+    	
+    	else if(hero instanceof Warrior) {
+    		image = "images/Heroes/DwarfMaleBoard.png";
+    	}
+    	
+    	else {
+    		image = "images/Heroes/MageMaleBoard.png";
+    	}
+    	
+    	
+    	playerBoardImage = new MinuetoImageFile(image).scale(300/ 671.0, 300/ 671.0);
     }
 
-    public static PlayerBoard getInstance() throws MinuetoFileException {
-        if(playerBoard == null)
-            playerBoard = new PlayerBoard();
+    public static PlayerBoard getInstance(Hero hero) throws IOException {
+        if(playerBoard == null) {
+        	playerBoard = new PlayerBoard(hero);
+        }
+            
+        
         return playerBoard;
     }
 
     public void draw() {
-
+    	if(flag) {
+    		gameStatus.screen.draw(playerBoardImage, 300, 100);
+    		gameStatus.screen.draw(spImage, 665, 205);
+    		gameStatus.screen.draw(wpImage, 795, 260);
+//            gameStatus.screen.draw(new MinuetoText("Strength Point: " + sp , font, MinuetoColor.WHITE), , 510);
+//            gameStatus.screen.draw(new MinuetoText("Will Power: " + wp , font, MinuetoColor.WHITE), 910, 530);
+    	}
+    	
+    	else {
+    		
+    	}
+    }
+    
+    public void update(Hero hero) {
+    	draw();
+    }
+    
+    public void toggleFlag() {
+    	if(flag == true) {
+    		flag = false;
+    	}
+    	
+    	else {
+    		flag = true;
+    	}
     }
 }
