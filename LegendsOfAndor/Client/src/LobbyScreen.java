e08be33c43d80bf742a92a6115a44f53cc94658e
jargonless <43076948+jargonless@users.noreply.github.com>
2020-03-12 08:35:07 -0400

    Update LobbyScreen.java

--- LegendsOfAndor/Client/src/LobbyScreen.java
+++ LegendsOfAndor/Client/src/LobbyScreen.java
@@ -93,25 +93,21 @@ public class LobbyScreen implements Inputtable, Serializable {
         isEasy = !isEasy;
         else if(button == MinuetoMouse.MOUSE_BUTTON_LEFT && clickedScale(ghettoText + 650,
                 325, ghettoText + 750, 225, x, y)) {
-        	chosenHero = "Warrior";
         	currentChar = 0;
         }
             
         else if(button == MinuetoMouse.MOUSE_BUTTON_LEFT && clickedScale(ghettoText + 800,
                 325, ghettoText + 900, 225, x, y)) {
         	currentChar = 1;
-        	chosenHero = "Archer";
         }
             
         else if(button == MinuetoMouse.MOUSE_BUTTON_LEFT && clickedScale(ghettoText + 950,
                 325, ghettoText + 1050, 225, x, y)) {
-        	chosenHero = "Dwarf";
         	currentChar = 2;
         }
             
         else if(button == MinuetoMouse.MOUSE_BUTTON_LEFT && clickedScale(ghettoText + 1100,
                 325, ghettoText + 1200, 225, x, y)) {
-        	chosenHero = "Mage";
         	currentChar = 3;
         }
     }
