98bb75100bc176d1e17e8f67efb7f7416304ce3b
jargonless <yixin.liu@mail.mcgill.ca>
2020-03-12 02:50:16 -0400

    Mapped all gors

--- LegendsOfAndor/Client/src/MonsterInitializer.java
+++ LegendsOfAndor/Client/src/MonsterInitializer.java
@@ -9,10 +9,17 @@ public class MonsterInitializer {
 
 	public static ArrayList<Monster> initializeMonsters() throws IOException {
         ArrayList<Monster> output = new ArrayList<>();
-        Monster m1 = new Gor(new MinuetoImageFile("images/Monsters/Gor.png"), 4);
-        Monster m2 = new Gor(new MinuetoImageFile("images/Monsters/Gor.png"), 5);
+        Monster m1 = new Gor(new MinuetoImageFile("images/Monsters/Gor.png"), 8);
+        Monster m2 = new Gor(new MinuetoImageFile("images/Monsters/Gor.png"), 20);
+        Monster m3 = new Gor(new MinuetoImageFile("images/Monsters/Gor.png"), 21);
+        Monster m4 = new Gor(new MinuetoImageFile("images/Monsters/Gor.png"), 26);
+        Monster m5 = new Gor(new MinuetoImageFile("images/Monsters/Gor.png"), 48);
         output.add(m1);
- //       output.add(m2);
+        output.add(m2);
+        output.add(m3);
+        output.add(m4);
+        output.add(m5);
+        
         for (Monster monster : output)
         {
         	GameScreen.tiles.get(monster.tile).addTileEntity(monster);
