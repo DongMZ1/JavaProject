1d2ba40d7322bddb118e3589e1dde56d2119e645
DongMZ1 <46273365+DongMZ1@users.noreply.github.com>
2020-04-19 00:49:36 -0400

    fix bug

--- LegendsOfAndor/Client/src/Castle.java
+++ LegendsOfAndor/Client/src/Castle.java
@@ -12,7 +12,7 @@ public class Castle implements Serializable{
 	
 	public void damage(Monster m) {
     	//TODO: Should decrease shield count of castle
-		if ((m instanceof Monster) {
+		if (m instanceof Monster) {
 			//UPDATE
 			System.out.println("Damaged castle by Gor");
 			health--;
